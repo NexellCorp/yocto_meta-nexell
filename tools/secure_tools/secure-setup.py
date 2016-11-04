301c713c6c5c0f03cb05f5e042378f89317bd7be
choonghyun-Jeon <suker@nexell.co.kr>
2016-11-04 13:55:54 +0900

    meta-nexell: tools: secure tools grouping
    The secure files grouped in a secure_tools dir
    when fusing about s5p6818 target board.
    
    Change-Id: I5d92e4558e7bc943c0584f7afbedc20243c5458a
    Signed-off-by: choonghyun-Jeon <suker@nexell.co.kr>

--- tools/secure_tools/secure-setup.py
+++ tools/secure_tools/secure-setup.py
@@ -5,12 +5,9 @@ import sys
 import os
 import fileinput
 
-OPTEE_BUILD_BB_FILE = "../meta-nexell/recipes-bsp/optee/optee-build_git.bb"
-OPTEE_LINUXDRIVER_BB_FILE = "../meta-nexell/recipes-bsp/optee/optee-linuxdriver_git.bb"
-OPTEE_SECURE_INC_FILE = "../meta-nexell/recipes-bsp/optee/optee-secure.inc"
-BL1_SECURE_INC_FILE = "../meta-nexell/recipes-bsp/bl1/bl1-secure.inc"
-
-def optee_build_setup(machineName) :
+def optee_build_setup(machineName,meta_nexell_path) :
+    OPTEE_BUILD_BB_FILE = meta_nexell_path+"/recipes-bsp/optee/optee-build_git.bb"
+    
     temp = "OPTEE_BUILD_TARGET_MACHINE=\""+machineName+"\""
     os.system('sed -i "/OPTEE_BUILD_TARGET_MACHINE=/d" ' + OPTEE_BUILD_BB_FILE)
     f = open(OPTEE_BUILD_BB_FILE,'a')
@@ -19,7 +16,9 @@ def optee_build_setup(machineName) :
     #    for line in fileinput.input(OPTEE_BUILD_BB_FILE, inplace = 1): 
     #        print line.replace("OPTEE_BUILD_TARGET_MACHINE=\"\"", "OPTEE_BUILD_TARGET_MACHINE=\""+machineName+"\""),
 
-def optee_secure_setup(option) :
+def optee_secure_setup(option,meta_nexell_path) :
+    OPTEE_SECURE_INC_FILE = meta_nexell_path + "/recipes-bsp/optee/optee-secure.inc"
+    
     if option == "ON":
         for line in fileinput.input(OPTEE_SECURE_INC_FILE, inplace = 1): 
             print line.replace("SECURE-OPTEE=\"SECURE_ON=FALSE\"", "SECURE-OPTEE=\"SECURE_ON=1\""),
@@ -27,7 +26,9 @@ def optee_secure_setup(option) :
         for line in fileinput.input(OPTEE_SECURE_INC_FILE, inplace = 1): 
             print line.replace("SECURE-OPTEE=\"SECURE_ON=TRUE\"", "SECURE-OPTEE=\"SECURE_ON=0\""),
 
-def bl1_secure_setup(option) :
+def bl1_secure_setup(option,meta_nexell_path) :
+    BL1_SECURE_INC_FILE = meta_nexell_path + "/recipes-bsp/bl1/bl1-secure.inc"
+    
     if option == "ON":
         for line in fileinput.input(BL1_SECURE_INC_FILE, inplace = 1): 
             print line.replace("SECURE-BL1=\"SECURE_ON=0\"", "SECURE-BL1=\"SECURE_ON=1\""),
@@ -35,13 +36,13 @@ def bl1_secure_setup(option) :
         for line in fileinput.input(BL1_SECURE_INC_FILE, inplace = 1): 
             print line.replace("SECURE-BL1=\"SECURE_ON=1\"", "SECURE-BL1=\"SECURE_ON=0\""),
            
-def main(arg1,arg2):
-    optee_build_setup(arg2)
-    optee_secure_setup(arg1)
-    bl1_secure_setup(arg1)
+def main(arg1,arg2,arg3):
+    optee_build_setup(arg2,arg3)
+    optee_secure_setup(arg1,arg3)
+    bl1_secure_setup(arg1,arg3)
     
 if __name__ == "__main__":
     try : 
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
     finally : 
         pass
