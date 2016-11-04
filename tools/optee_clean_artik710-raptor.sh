9c58fce84132311f1bd710e4983dce2ff51bced7
choonghyun-Jeon <suker@nexell.co.kr>
2016-11-04 15:07:59 +0900

    meta-nexell: tools: optee_clean script simplified
    Simplified the files that existed on each board to one file optee_clean.sh
    
    Change-Id: I6375b18ec3df47e83b3e6de5b594eab28f25700f
    Signed-off-by: choonghyun-Jeon <suker@nexell.co.kr>

--- tools/optee_clean_artik710-raptor.sh
+++
@@ -1,3 +0,0 @@
-rm -rf OPTEE_PRE_OPERATION_DONE
-
-bitbake -c cleansstate arm-trusted-firmware l-loader optee-build optee-client optee-os optee-test optee-linuxdriver s5p6818-artik710-raptor-bl1 s5p6818-artik710-raptor-uboot
--- tools/optee_clean_avn-ref.sh
+++
@@ -1,3 +0,0 @@
-rm -rf OPTEE_PRE_OPERATION_DONE
-
-bitbake -c cleansstate arm-trusted-firmware l-loader optee-build optee-client optee-os optee-test optee-linuxdriver s5p6818-avn-ref-bl1 s5p6818-avn-ref-uboot
