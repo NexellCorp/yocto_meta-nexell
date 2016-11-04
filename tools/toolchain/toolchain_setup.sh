30676260ea038fc79d073a815fd1f08499a60a78
choonghyun-Jeon <suker@nexell.co.kr>
2016-11-04 15:11:45 +0900

    meta-nexell: tools: toolchain_setup.sh move location
    The toolchain_setup.sh script file move to toolchain dir.
    
    Change-Id: I3a33e98dd9a89a01593138bd62a691360b7bcdc2
    Signed-off-by: choonghyun-Jeon <suker@nexell.co.kr>

--- tools/toolchain/toolchain_setup.sh
+++ tools/toolchain/toolchain_setup.sh
@@ -1,19 +1,25 @@
-mkdir -p tmp/work
+#SRC_PATH ==> meta-nexell abs path
+#DEST_PATH ==> build-xxx-xxx/tmp/work abs path
+
+SRC_PATH=$1
+DEST_PATH=$2
+
+mkdir -p ${DEST_PATH}
 echo -e "\033[0;33m Copying....\033[0m"
 echo -e "\033[0;33m     ARM 32bit tool chain copy to tmp/work, It need to be optee build \033[0m"
 echo -e "\033[0;33m     please wait...\033[0m"
-cp -an ../meta-nexell/tools/toolchain/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf tmp/work/ 
+cp -an ${SRC_PATH}/tools/toolchain/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf ${DEST_PATH}
 
 echo -e "\033[0;33m     ARM 64bit non-elf tool chain copy to tmp/work, It need to be bl1-artik7 build \033[0m"
 echo -e "\033[0;33m     please wait...\033[0m"
-cp -an ../meta-nexell/tools/toolchain/gcc-linaro-aarch64-none-elf-4.8-2014.04_linux tmp/work/
+cp -an ${SRC_PATH}/tools/toolchain/gcc-linaro-aarch64-none-elf-4.8-2014.04_linux ${DEST_PATH}
 
 echo -e "\033[0;33m     arm-eabi- tool chain copy to tmp/work, It need to be bl1-artik7 build \033[0m"
 echo -e "\033[0;33m     please wait...\033[0m"
-cp -an ../meta-nexell/tools/toolchain/arm-eabi-4.8 tmp/work/
+cp -an ${SRC_PATH}/tools/toolchain/arm-eabi-4.8 ${DEST_PATH}
 
 echo -e "\033[0;33m     arm-cortex_a9-eabi- tool chain copy to tmp/work, for u-boot-avn- build \033[0m"
 echo -e "\033[0;33m     please wait...\033[0m"
-cp -an ../meta-nexell/tools/toolchain/arm-cortex_a9-eabi-4.7-eglibc-2.18 tmp/work/
+cp -an ${SRC_PATH}/tools/toolchain/arm-cortex_a9-eabi-4.7-eglibc-2.18 ${DEST_PATH}
 
 echo -e "\033[0;33m Done!\033[0m"
