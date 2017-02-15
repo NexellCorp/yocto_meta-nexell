38bb1715a10afe411d3410e5392e53b34538d43c
jhkim <jhkim@nexell.co.kr>
2017-02-15 17:07:44 +0900

    meta-nexell: change codec(tlv320aic32x4) amixer control for smart-voice
    This patch change codec(tlv320aic32x4) amixer control  for smart-voice
    
    - 'HP Driver Gain' : 1 -> 6   (0 dB)
    - 'PCM' gain 	   : 0 -> 116 (-5.50 dB)
    - Use HPL/R output power with LDOIN (replace AVDD)
      : 'HPL/HPR Powered Supply' : LDOIN
      : 'LDOIN Input Range'      : '1.8V to 3.6V'
      : 'Output Common Mode'     : "1.65V"
    
    Change-Id: Ic1ad47a04b76fb01e054ea8a009efd06aa27b99e
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- recipes-multimedia/amixer-conf/files/amixer-conf.sh
+++ recipes-multimedia/amixer-conf/files/amixer-conf.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-amixer sset 'HPL Output Mixer L_DAC' on
-amixer sset 'HPR Output Mixer R_DAC' on
-amixer sset 'HP DAC' on on
-amixer sset 'HP Driver Gain' 1
+amixer sset 'HPL Output Mixer L_DAC' on;
+amixer sset 'HPR Output Mixer R_DAC' on;
+amixer sset 'HP DAC' on on;
+# HPL 6 -> 0 DB
+amixer sset 'HP Driver Gain' 6;
+# Digital Volume
+amixer sset 'PCM' 116;
+# Reset BIAS
+amixer sset 'Output Common Mode' "1.65V"
+amixer sset 'LDOIN Input Range' '1.8V to 3.6V'
+amixer sset 'HPL/HPR Powered Supply' LDOIN
+
