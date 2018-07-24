159efa0feddbd5a4ff7770901b015aee9410051a
Lee GeunJeong <kjlee@nexell.co.kr>
2018-07-24 10:10:20 +0900

    meta-nexell: Update adb scripts
    Modify the script file for correct operation.
    
    Change-Id: If5698bee5d46db04c0467da603e6e48769ce6207
    Signed-off-by: Lee GeunJeong <kjlee@nexell.co.kr>
    
    Former-commit-id: 5f98f93d8f7e96c13ffb6beedd80590f908a14ff

--- recipes-android/android-tools/files/start_adbd.sh
+++ recipes-android/android-tools/files/start_adbd.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-
-mount -t configfs none /sys/kernel/config
-
 GADGET_DIR=/sys/kernel/config/usb_gadget/g1
 SERIAL_PATH=/proc/device-tree/serial-number
 
-#Mount ConfigFS and create Gadget
+# Set otg device mode for daudio_ref
+echo device > /sys/devices/platform/c0000000.soc/c0040000.dwc2otg/sel_dr_mode
+
+# Mount configFS and create gadget
 if ! [ -d "${GADGET_DIR}" ]; then
 	mkdir ${GADGET_DIR}
 fi
 
-#Set default Vendor and Product IDs for now
+# Set default vendor and product IDs for now
 echo 0x18D1 > ${GADGET_DIR}/idVendor
 echo 0x0001 > ${GADGET_DIR}/idProduct
 
-#Create English strings and add random deviceID
+# Create english strings and add random deviceID
 if ! [ -d "${GADGET_DIR}/strings/0x409" ]; then
 	mkdir ${GADGET_DIR}/strings/0x409
 fi
 
-#Set serial number
+# Set serial number
 if [ -f ${SERIAL_PATH} ]; then
 	cat ${SERIAL_PATH} > ${GADGET_DIR}/strings/0x409/serialnumber
 else
@@ -28,7 +28,7 @@ fi
 echo NEXELL > ${GADGET_DIR}/strings/0x409/manufacturer
 cat /etc/hostname > ${GADGET_DIR}/strings/0x409/product
 
-#Create gadget configuration
+# Create gadget configuration
 if ! [ -d "${GADGET_DIR}/configs/c.1" ]; then
 	mkdir ${GADGET_DIR}/configs/c.1
 	mkdir ${GADGET_DIR}/configs/c.1/strings/0x409
@@ -36,9 +36,8 @@ fi
 echo 120 > ${GADGET_DIR}/configs/c.1/MaxPower
 echo "Conf 1" > ${GADGET_DIR}/configs/c.1/strings/0x409/configuration
 
-
-#Create Adb FunctionFS function
-#And link it to the gadget configuration
+# Create adb functionFS function
+# And link it to the gadget configuration
 if ! [ -d "${GADGET_DIR}/functions/ffs.adb" ]; then
 	mkdir ${GADGET_DIR}/functions/ffs.adb
 	ln -s ${GADGET_DIR}/functions/ffs.adb ${GADGET_DIR}/configs/c.1/ffs.adb
@@ -49,10 +48,10 @@ if ! [ -d "/dev/usb-ffs/adb" ]; then
 	mount -o uid=2000,gid=2000 -t functionfs adb /dev/usb-ffs/adb
 fi
 
-#Start adbd application
+# Start adbd application
 /usr/bin/adbd&
 echo $! > /run/adbd.pid
-sleep 2
+sleep 1
 
 if [ "`cat ${GADGET_DIR}/UDC`" == "c0040000.dwc2otg" ]; then
 	echo "" > ${GADGET_DIR}/UDC
