de64e518a304085fdd69c50f0ed6297032f99426
mcjino <allan.park@nexell.co.kr>
2018-08-24 18:25:22 +0900

    meta-nexell: cluster: fix the cluster.init.d
    This patch add the cluster.init.d on nx_3d_avm.
    
    Change-Id: Id3c84f7ed8217b78f01df062e91f8bd918abc331
    Signed-off-by: mcjino <allan.park@nexell.co.kr>
    
    Former-commit-id: 875316de41ed6bacb56b0eea440bad34214e3352

--- recipes-sdk/nexell-cluster-sdk/files/script/cluster.init.d
+++ recipes-sdk/nexell-cluster-sdk/files/script/cluster.init.d
@@ -19,12 +19,21 @@ case "$1" in
   start)
         export LD_LIBRARY_PATH=/nexell/cluster
         export XDG_RUNTIME_DIR=/run/user/0
+
+		echo "Autorun TiaPlayer"
+		/sbin/ip link set can0 up type can bitrate 500000
+		/sbin/ip link set can0 up type can bitrate 500000
+		echo "enable can0" 
+		echo "start nx_3d_avm"
+		/nexell/avm_app/nx_3d_avm &
+		echo "start TiaPlayer"
         /nexell/cluster/TiaPlayer.elf -w1920 -h720 -c/nexell/cluster/ -r/nexell/cluster/
   ;;
 
   stop)
         echo "Stopping Weston"
         killproc TiaPlayer.elf
+        killproc nx_3d_avm
   ;;
 
   restart)
