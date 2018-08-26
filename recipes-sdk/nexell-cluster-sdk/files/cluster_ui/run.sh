11d75bfc39ae8afb0dfac8b2b0b9da65636e4354
mcjino <allan.park@nexell.co.kr>
2018-08-26 14:25:22 +0900

    meta-nexell: cluster: sync to 20180825 application
    This patch sync to 20180825 application.
    
    Change-Id: I16849392377c6b2eaac34e74052ad9e97643be78
    Signed-off-by: mcjino <allan.park@nexell.co.kr>
    
    Former-commit-id: e79522c88d614af7bdce49548e466cac0aa6f78d

--- recipes-sdk/nexell-cluster-sdk/files/cluster_ui/run.sh
+++ recipes-sdk/nexell-cluster-sdk/files/cluster_ui/run.sh
@@ -2,16 +2,12 @@
 
 echo "Demo TiaPlyaer"
 
-export XDG_RUNTIME_DIR=/run/wayland
-export LD_LIBRARY_PATH=$PWD
+export LD_LIBRARY_PATH=/nexell/cluster
 
 echo $LD_LIBRARY_PATH
 
-export XDG_RUNTIME_DIR=/run/wayland
-export `dbus-launch`
-
-chmod +x ./TiaPlayer.elf
-./TiaPlayer.elf -w1920 -h720 -c./ -r./
+chmod +x /nexell/cluster/TiaPlayer.elf
+/nexell/cluster/TiaPlayer.elf -w1920 -h720 -c/nexell/cluster -r/nexell/cluster
 
 
 
