183a61d2bdf3cbca9931f92a36c3edda613de3ab
jhkim <jhkim@nexell.co.kr>
2019-01-09 19:22:58 +0900

    meta-nxp3220: adds 'android-tools' append files to run usb adbd service
    This patch adds 'android-tools' append files to run usb adbd service
    adds:
    - android-tools-adbd.service : run 'start_adbd.sh'
    - start_adbd.sh : execute 'adbd'
    - stop_adbd.sh : stop 'adbd'
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ied45f344cb56aaf11138905a84581b3eee175fdf

---
+++ meta-nxp3220/recipes-bbappend/android-tools/files/stop_adbd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+CONFIG_DIR=/sys/kernel/config/usb_gadget/g1
+CONFIG_FUNC=ffs.adb
+TASK_PID=`pidof adbd`
+
+kill $TASK_PID
+
+if [ -d "${CONFIG_DIR}/functions/$CONFIG_FUNC" ]; then
+    rm ${CONFIG_DIR}/configs/c.1/$CONFIG_FUNC
+    rmdir ${CONFIG_DIR}/configs/c.1/strings/0x409
+    rmdir ${CONFIG_DIR}/configs/c.1
+    rmdir ${CONFIG_DIR}/functions/$CONFIG_FUNC
+    rmdir ${CONFIG_DIR}/strings/0x409
+fi
+
+if [ -d "/dev/usb-ffs/adb" ]; then
+    umount /dev/usb-ffs/adb
+    rmdir /dev/usb-ffs/adb
+fi
