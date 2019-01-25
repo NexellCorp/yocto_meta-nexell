9c7228fb8a01867917a286b665d68318bffb3236
justin <justin@nexell.co.kr>
2019-01-25 20:42:20 +0900

    meta-nexell: bitminer: change default network state
    Change-Id: Ic2e550d8aa9f4d1c446e1fc7101c0cbac843b793
    Signed-off-by: justin <justin@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/yocto_meta-nexell/+/459244

--- meta-nexell-distro/recipes-miners/cgminer/files/checkminerfs.sh
+++ meta-nexell-distro/recipes-miners/cgminer/files/checkminerfs.sh
@@ -8,8 +8,6 @@ if [ ! "$CONFIG_MMC_FS" ]; then
 	sync;
 	mount /dev/mmcblk0p5 /config
 	sync;
-	cat /sys/class/net/eth0/address > /config/eth0addr
-	sync;
 	REBOOT_FLAG=1
 fi
 
@@ -19,11 +17,18 @@ fi
 #	REBOOT_FLAG=1
 #fi
 
+HOSTNAME_PREFIX=`/bin/hostname | awk -F- '{print $1$2}'`
+if [ "$HOSTNAME_PREFIX" != "poc2-ctrlbd" ]; then
+	ENVMAC=`/sbin/fw_printenv | grep ethaddr=a4 | awk -F= '{print $2}'| tr -d : | tr 'a-z' 'A-Z'`
+	echo "poc2-ctrlbd-$ENVMAC" > /etc/hostname
+	REBOOT_FLAG=1
+fi
+
 if [ $REBOOT_FLAG -ne 0 ]; then
 #	echo "/dev/mmcblk0p5	/config			auto	rw,noexec	0 0" >> /etc/fstab
 #	echo "/dev/mmcblk0p6	/var/mmclog		auto	rw,noexec	0 0" >> /etc/fstab
 	sync;
-	/sbin/reboot
+	/sbin/reboot -f
 fi
 
 CONFIG_MMC_FS_MOUNT=`/bin/mount | /bin/grep mmcblk0p5`
@@ -32,18 +37,86 @@ if [ ! "$CONFIG_MMC_FS_MOUNT" ]; then
 	sync
 fi
 
-if [ ! -e /config/cgminer.conf ]; then
-	cp /etc/cgminer.conf.factory /config/cgminer.conf
-	sync
-fi
+#MACADDRUSER_FLAG=`cat /config/cgminer.conf | grep macxxxxxx`
+#if [ "$MACADDRUSER_FLAG" ]; then
+#	cp /etc/cgminer.conf.factory /config/cgminer.conf
+#fi
 
-if [ ! -e /config/eth0addr ]; then
-	cat /sys/class/net/eth0/address > /config/eth0addr
-	sync;
+#if [ ! -e /etc/systemd/network/10-eth.network ]; then
+#	cp /etc/10-eth.network.factory /etc/systemd/network/10-eth.network
+#	sync
+#fi
+
+TEST_MODE=`cat /sys/class/gpio/gpio15/value`
+if [ "$TEST_MODE" = "0" ]; then
+	echo "test mode"
+	exit
 fi
 
 if [ ! -e /etc/systemd/network/10-eth.network ]; then
-	cp /etc/10-eth.network.factory /etc/systemd/network/10-eth.network
+	/usr/sbin/avahi-autoipd -wD eth0
+else
+	/sbin/ifup eth0
+fi
+
+TIMEOUT_MAC=0
+while [ $(cat /sys/class/net/eth0/address | awk -F: '{print $1 $2 $3}') != "a45f9b" ]
+do
+	sleep 1
 	sync
+	TIMEOUT_MAC=$(($TIMEOUT_MAC+1))
+	if [ $TIMEOUT_MAC -lt 100 ]; then
+		/usr/bin/logger -p local0.error "MAC address is not normal"
+		cat /sys/class/net/eth0/address | /usr/bin/logger -p local0.error
+	fi
+done
+
+if [ ! -e /config/eth0addr ]; then
+	cat /sys/class/net/eth0/address > /config/eth0addr
+fi
+
+MYMAC=`cat /sys/class/net/eth0/address | tr -d : | tr 'a-z' 'A-Z'`
+
+DEFAULT_MACADDRUSER_FLAG=`cat /etc/cgminer.conf.factory | grep macxxxxxx`
+if [ "$DEFAULT_MACADDRUSER_FLAG" ]; then
+	sed -i -e 's/macxxxxxx/'"$MYMAC"'/g' /etc/cgminer.conf.factory
+fi
+
+create_default_conf_file()
+{
+(
+cat <<'EOF'
+{
+"pools" : [
+{
+"url" : "stratum+tcp://hansandopool.com:3333",
+"user" : "12ships_DaeJeon.macxxxxxx",
+"pass" : "x"
+},
+{
+"url" : "stratum+tcp://hansandopool.com:3333",
+"user" : "12ships_DaeJeon.macxxxxxx",
+"pass" : "x"
+}
+]
+,
+"api-listen" : true,
+"api-network" : true,
+"api-allow" : "W:0/0",
+"temp-cutoff" : 100,
+"tsb1101-volt": "400"
+}
+
+EOF
+) > /etc/cgminer.conf.factory
+}
+
+if [ ! -e /config/cgminer.conf ]; then
+	if [ ! -e /etc/cgminer.conf.factory ]; then
+		create_default_conf_file
+		MACAD=$(cat /sys/class/net/eth0/address | tr -d : | tr 'a-z' 'A-Z')
+		sed -i -e 's/macxxxxxx/'"$MACAD"'/g' /etc/cgminer.conf.factory
+	fi
+	cp /etc/cgminer.conf.factory /config/cgminer.conf
 fi
 
