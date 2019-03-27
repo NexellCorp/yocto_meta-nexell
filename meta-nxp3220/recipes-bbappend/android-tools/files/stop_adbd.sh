c552a6aa8ecc41a73aaa769645ac37c083ad7a49
jhkim <jhkim@nexell.co.kr>
2019-03-27 15:49:19 +0900

    meta-nxp3220: Fix data abort when stop_adbd.sh
    This patch fix data abort when stop_adbd.sh.
    
    if executed 'umount /dev/usb-ffs/adb' after
    'rmdir ${CONFIG_DIR}/functions/$CONFIG_FUNC', occurs data abort when usb release
    
    umount /dev/usb-ffs/adb:
    	- call ffs_release_dev function in usb driver
    
    rmdir ${CONFIG_DIR}/functions/$CONFIG_FUNC
    	- call ffs_free_inst function in usb driver
    
    Change-Id: I61bdb5347a2e53c71df826a5de124b3ee6fa8420
    
    ----------------------------------------------------
    Unable to handle kernel paging request at virtual address ffff911a
    pgd = d930c000
    [ffff911a] *pgd=5afde861, *pte=00000000, *ppte=00000000
    Internal error: Oops: 80000007 [#1] PREEMPT SMP ARM
    Modules linked in:
    CPU: 0 PID: 1156 Comm: umount Not tainted 4.14.73 #89
    Hardware name: NEXELL (Device Tree Support)
    task: d9a7ba80 task.stack: d9170000
    PC is at 0xffff911a
    LR is at ffs_release_dev+0xfc/0x11c
    ----------------------------------------------------
    
    Change-Id: I82bf4f16af25f730ce2d66898ada0be004f6335e
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- meta-nxp3220/recipes-bbappend/android-tools/files/stop_adbd.sh
+++ meta-nxp3220/recipes-bbappend/android-tools/files/stop_adbd.sh
@@ -5,15 +5,17 @@ TASK_PID=`pidof adbd`
 
 kill $TASK_PID
 
+# must be unmount adb before
+# execute 'rmdir ${CONFIG_DIR}/functions/$CONFIG_FUNC'
+if [ -d "/dev/usb-ffs/adb" ]; then
+    umount /dev/usb-ffs/adb
+    rmdir /dev/usb-ffs/adb
+fi
+
 if [ -d "${CONFIG_DIR}/functions/$CONFIG_FUNC" ]; then
     rm ${CONFIG_DIR}/configs/c.1/$CONFIG_FUNC
     rmdir ${CONFIG_DIR}/configs/c.1/strings/0x409
     rmdir ${CONFIG_DIR}/configs/c.1
     rmdir ${CONFIG_DIR}/functions/$CONFIG_FUNC
     rmdir ${CONFIG_DIR}/strings/0x409
-fi
-
-if [ -d "/dev/usb-ffs/adb" ]; then
-    umount /dev/usb-ffs/adb
-    rmdir /dev/usb-ffs/adb
 fi
