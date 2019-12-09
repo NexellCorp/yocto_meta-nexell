979251d9122f7221de29c3e170481071526abd2f
allan park <allan.park@nexell.co.kr>
2019-12-10 08:56:17 +0900

    meta-nexell: change QT_QPA_EGLFS_INTEGRATION=none.to eglfs_kms
    It need to work on qt apps which can work on -platform eglfs
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: Ibbeceef4752e8962b17436f838e535f887b6a91d

--- meta-nexell-distro/recipes-graphics/weston/weston-conf/wayland_env.sh
+++ meta-nexell-distro/recipes-graphics/weston/weston-conf/wayland_env.sh
@@ -7,7 +7,7 @@ export XDG_RUNTIME_DIR=/tmp/wayland-runtime-dir
 export SDL_VIDEODRIVER=wayland
 export WAYLAND_DISPLAY=wayland-0
 export WS_CALUDEV_FILE=/etc/udev/rules.d/ws-calibrate.rules
-export QT_QPA_EGLFS_INTEGRATION=none
+export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 
 if ! test -d "${XDG_RUNTIME_DIR}"; then
        mkdir -p "${XDG_RUNTIME_DIR}"
